2fad2359ea2c2e382f4e2f287f415fd1a9937d52
ZiaurRahman744 <ziaurrahman744@gmail.com>
2026-09-18 17:09:35 +0600

    update

--- .eleventy.js
+++ .eleventy.js
@@ -5,6 +5,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("css");
   eleventyConfig.addPassthroughCopy("js");
   eleventyConfig.addPassthroughCopy("pages");
+  eleventyConfig.addPassthroughCopy("admin");
   eleventyConfig.addPassthroughCopy("index.html");
   eleventyConfig.addPassthroughCopy("services/third-party-inspection.html");
 
